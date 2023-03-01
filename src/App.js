2e198f0eb5d1532304b457e1ebcec77f15613e60
Sanjin888 <sanjinkurtalija@gmail.com>
2023-03-02 00:50:57 +0100

    Initial development

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Square from "./Components/Square";
 
 function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+
   return (
     <div className="App">
+      <div className="board">
+        <div className="row">
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              alert(0);
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              alert(1);
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              alert(2);
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
